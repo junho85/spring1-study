263439067e996ff718e6e84b6d7a8b12b492fd79
june.kim <junho85@gmail.com>
2020-06-21 21:33:13 +0900

    afterPropertiesSet

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,4 +1,10 @@
-public class Hello {
+import org.springframework.beans.factory.InitializingBean;
+
+public class Hello implements InitializingBean {
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("== afterPropertiesSet ==");
+    }
+
     public void hello() {
         System.out.println("hello");
     }
